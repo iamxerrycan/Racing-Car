3861fca0106f06298c4c183a950ce78d55cbd1b3
Rajshish Singh <80442999+rajshish333@users.noreply.github.com>
2022-12-07 21:39:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// var audio = document.createElement('audio');
+// audio.setAttribute('src', 'sound.mp3');
+// audio.loop = true;
+// audio.play();
+
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'sound.mp3');
+audio.loop = true;
+audio.play();
+
+
+// // audio.play();
+// i implementend audio by js
